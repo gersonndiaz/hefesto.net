e2c70ad8389b0f86954e983f18ac94ff3fda87a6
Gerson Díaz <gersonn.diaz@gmail.com>
2021-02-03 21:53:12 -0300

    Se incorpora objeto ResponseGenericModel.cs como objeto estandar para la respuesta de una solicitud http

---
+++ Hefesto/Response/ResponseGenericModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hefesto.Response
+{
+    public class ResponseGenericModel
+    {
+        public string status { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+        public int httpCode { get; set; }
+        public string urlRedirect { get; set; }
+        public bool? reload { get; set; }
+        public object data { get; set; }
+    }
+
+    public static class StatusResponseCodes
+    {
+        public const string
+            StatusResponseSuccess = "success",
+            StatusResponseError = "error",
+            StatusResponseWarn = "warn";
+    }
+}
