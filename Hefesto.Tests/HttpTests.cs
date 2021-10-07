a52d0a3248259bfe42cb4c4099a0eaa5d4b75cac
Gerson Díaz <gersonn.diaz@gmail.com>
2021-10-06 23:05:31 -0300

    Se incorpora validación de Http

---
+++ Hefesto.Tests/HttpTests.cs
@@ -0,0 +1,30 @@
+﻿using Hefesto.Check;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hefesto.Tests
+{
+    public class HttpTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void HttpTest()
+        {
+            bool b1= HttpUtil.urlExist("https://www.ckelar.cl/");
+            bool b2 = HttpUtil.urlExist("http://ckelarsoftware.cl/");
+            //bool b3 = HttpUtil.urlExist("http://ckelarsoftware.cl/Prueba");
+            bool b4 = HttpUtil.urlExist("https://www.ckelar.cl/web/wp-content/uploads/2019/04/cropped-ckelar4_t.png");
+            
+            Assert.IsTrue(b1);
+            Assert.IsTrue(b2);
+            //Assert.IsFalse(b3);
+            Assert.IsTrue(b4);
+        }
+    }
+}
---
+++ Hefesto/Check/HttpUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Hefesto.Check
+{
+    /// <summary>
+    /// Esta clase permite realizar validaciones HTTP
+    /// </summary>
+    /// <summary lang="en-US">
+    /// This class allows to perform HTTP validations
+    /// </summary>
+    public class HttpUtil
+    {
+        /// <summary>
+        /// Comprueba si la URL ingresada existe
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool urlExist(string url)
+        {
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
